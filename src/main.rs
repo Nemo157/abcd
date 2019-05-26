6acfe448cd448c8adb80b45b1e698d2ad651565b
Wim Looman <wim@nemo157.com>
2019-05-26 18:53:39 +0200

    Switch from slog-term to slog-bunyan

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use slog::{o, Drain};
 
 #[runtime::main(runtime_tokio::Tokio)]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
+    let drain = slog_async::Async::new(slog_bunyan::default(std::io::stdout()).fuse()).build().fuse();
 
     let log = slog::Logger::root(drain, o!("service" => "abcd"));
 
@@ -18,7 +16,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let request_id = || bs58::encode(uuid::Uuid::new_v4().as_bytes()).into_string();
 
     app.middleware(tide_slog::PerRequestLogger::new(log, move |logger| {
-        logger.new(o!("request" => request_id()))
+        logger.new(o!("req_id" => request_id()))
     }));
     app.middleware(tide_slog::SetSlogScopeLogger);
     app.middleware(tide_slog::RequestLogger);
